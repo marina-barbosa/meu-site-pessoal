c040cfed66943b95be1fda5d74de595f38126127
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-01-08 06:23:13 -0300

    efeito mudar tab

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -92,3 +92,36 @@ function navContato() {
 //     button.addEventListener('click', handleNavigation);
 //   });
   
+
+// NAVBAR COM Z-INDEX E EFEITOS DATA-TAB
+
+let home = document.querySelector('#home');
+let skill = document.querySelector('#skills');
+let project = document.querySelector('#projetos');
+let experience = document.querySelector('#exp');
+let contact = document.querySelector('#contato');
+
+let aHref = document.querySelectorAll('a');
+
+let active = '#home';
+let zIndex = 600;
+
+aHref.forEach(a => {
+    a.addEventListener('click', function(event){
+        let tab = a.dataset.tab;
+        if(tab !== null && tab !== active){
+
+            let activeOld = document.querySelector('.tab.active');
+            if(activeOld) activeOld.classList.remove('active');
+            active = tab;
+
+            let tabActive = document.getElementById(active);
+            zIndex++;
+            tabActive.style.zIndex = zIndex;
+            tabActive.style.setProperty('--x', event.clientX + 'px');
+            tabActive.style.setProperty('--y', event.clientY + 'px');
+            tabActive.classList.add('active');
+        }
+    })
+})
+
