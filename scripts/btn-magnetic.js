7d43c9fca0162a797e09c7e5a8123a28b4c8c8f3
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-01-12 22:49:35 -0300

    teste btn magnetic

---
+++ scripts/btn-magnetic.js
@@ -0,0 +1,28 @@
+let circle = document.querySelector('.circle');
+
+let btnMagnetic = document.querySelectorAll('.btn-magnetic').forEach(btnMagnetic => {
+    // magnetismo
+    btnMagnetic.addEventListener('mousemove', (event) => {
+        let x = event.offsetX;
+        let y = event.offsetY;
+        let btnWidth = btnMagnetic.clientWidth;
+        let btnHeight = btnMagnetic.clientHeight;
+        let transX = (x - btnWidth / 2);
+        let transY = (y - btnHeight / 2);
+        btnMagnetic.style.transform = `translateX(${transX}px) translateY(${transY}px)`;
+
+    })
+    // reset position
+    btnMagnetic.addEventListener('mouseout', (event) => {
+        btnMagnetic.style.transform = '';
+    })
+    // circulo hover
+    btnMagnetic.addEventListener('mousemove', (event) => {
+        if (circle) {
+            circle.style.left = (event.offsetX - 25) + 'px';
+            circle.style.top = (event.offsetY - 25) + 'px';
+        }
+    });
+
+})
+
