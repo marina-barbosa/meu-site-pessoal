552315712aea55ba3ee7282c5d8aa11cd2857103
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-01-13 07:32:37 -0300

    teste magnetic

--- scripts/btn-magnetic.js
+++ scripts/btn-magnetic.js
@@ -2,20 +2,20 @@ let circle = document.querySelector('.circle');
 
 let btnMagnetic = document.querySelectorAll('.btn-magnetic').forEach(btnMagnetic => {
     // magnetismo
-    // btnMagnetic.addEventListener('mousemove', (event) => {
-    //     let x = event.offsetX;
-    //     let y = event.offsetY;
-    //     let btnWidth = btnMagnetic.clientWidth;
-    //     let btnHeight = btnMagnetic.clientHeight;
-    //     let transX = (x - btnWidth / 2);
-    //     let transY = (y - btnHeight / 2);
-    //     btnMagnetic.style.transform = `translateX(${transX}px) translateY(${transY}px)`;
+    btnMagnetic.addEventListener('mousemove', (event) => {
+        let x = event.offsetX;
+        let y = event.offsetY;
+        let btnWidth = btnMagnetic.clientWidth;
+        let btnHeight = btnMagnetic.clientHeight;
+        let transX = (x - btnWidth / 2);
+        let transY = (y - btnHeight / 2);
+        btnMagnetic.style.transform = `translateX(${transX}px) translateY(${transY}px)`;
 
-    // })
-    // // reset position
-    // btnMagnetic.addEventListener('mouseout', (event) => {
-    //     btnMagnetic.style.transform = '';
-    // })
+    })
+    // reset position
+    btnMagnetic.addEventListener('mouseout', (event) => {
+        btnMagnetic.style.transform = '';
+    })
     // circulo hover
     btnMagnetic.addEventListener('mousemove', (event) => {
         if (circle) {
