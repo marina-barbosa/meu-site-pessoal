760ae9b2b8ffe716123db35975c50dd51cea96b3
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-01-13 15:27:03 -0300

    add typed js

---
+++ scripts/config-typed.js
@@ -0,0 +1,7 @@
+let typingText = new Typed('#text-typed', {
+    strings: ['Desenvolvedora BackEnd', 'Desenvolvedora FullStack'],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000
+})
